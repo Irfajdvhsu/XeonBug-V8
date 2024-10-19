16eb3df2127dba661138ad31579529d550caaed0
Irfajdvhsu <azadindia40@gmail.com>
2024-10-19 23:59:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,26 +10,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
+global.ownernomer = "917978629189"
+global.ownername = "Official Smmpanelcheapest"
+global.ytname = "smmpanelcheapest.shop"
 global.socialm = "GitHub: GlobalTechInfo"
-global.location = "Pakistan, Karachi, Karachi"
+global.location = "India, Kolkata, Ponkamra"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '917978629189'  //creator number
+global.ownername = 'Official Smmpanelcheapest' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'smmpanelcheapest.shop\n\nContact: +917978629189'
 
 //console view/theme
 global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'http://wa.me/+917978629189'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
